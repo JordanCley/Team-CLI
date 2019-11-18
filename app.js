85db4c12b8984750943aaffc04670ce5944c07c6
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-11-18 10:00:10 -0800

    deleted templates and created variables to save string literals to, then concatinated them and used fs.write to created team.html file in output

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+let htmlIntern = "";
+let htmlEngineer = "";
+let htmlManager = "";
 
 
 // const readFileAsync = util.promisify(fs.readFile);
@@ -127,7 +130,54 @@ function generateManager(answers){
 `
 }
 
+const headerHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Main</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="jumbotron">
+      <h1 class="display-2 text-center">Engineering Team</h1>
+    </div>
+
+    <div class="card-deck" style="width: 80rem; margin: auto;">`;
+
+    const footerHtml = `</div>
+
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+    </body>
+    </html>`;
+
 function generateFinal(){
+  const combinedHTML = headerHtml + htmlEngineer + htmlIntern + htmlManager + footerHtml;
+  fs.writeFile('./output/team.html', combinedHTML, (err) => {
+    if(err) throw err;
+    console.log("team.html was created");
+  })
+  // console.log(combinedHTML);
 }
 
 function deleteFiles(){
@@ -151,33 +201,34 @@ function promptUser() {
     if(answers.title == 'Engineer'){
       let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
       engineer.getRole();
-      let html = generateEngineer(answers);
-      fs.appendFile('./templates/engineer.html', html, (err) => {
-        if(err) throw err;
-        console.log("file has been saved");
-      })
+      htmlEngineer += generateEngineer(answers);
+      // fs.appendFile('./templates/engineer.html', html, (err) => {
+      //   if(err) throw err;
+      //   console.log("file has been saved");
+      // })
       // teamMembers.push(engineer);
       // console.log(teamMembers);
 
     } if(answers.title == 'Intern'){
       let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
       intern.getRole();
-      let html = generateIntern(answers);
-      fs.appendFile('./templates/intern.html', html, (err) => {
-        if(err) throw err;
-        console.log("file has been saved");
-      })
+      htmlIntern += generateIntern(answers);
+     
+      // fs.appendFile('./templates/intern.html', html, (err) => {
+      //   if(err) throw err;
+      //   console.log("file has been saved");
+      // })
       // teamMembers.push(intern);
       // console.log(teamMembers);
 
     } if(answers.title == 'Manager'){
       let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
       manager.getRole();
-      let html = generateManager(answers);
-      fs.appendFile('./templates/manager.html', html, (err) => {
-        if(err) throw err;
-        console.log("file has been saved");
-      })
+      htmlManager += generateManager(answers);
+      // fs.appendFile('./templates/manager.html', html, (err) => {
+      //   if(err) throw err;
+      //   console.log("file has been saved");
+      // })
       // teamMembers.push(manager);
       // console.log(teamMembers);
     }
@@ -192,7 +243,12 @@ async function init(){
     const answers = await promptUser();
     if(answers.addMember){
       init();
+    } else {
+      generateFinal();
     }
+
+    
+    
     // await generateFinal();
     // console.log(teamMembers[0].name);
     
